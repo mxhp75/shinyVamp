7608d722a674e48e999df630c446b1d519874050
Melanie Smith <a1627211@adelaide.edu.au>
2021-03-15 13:45:57 +1030

    draculR.R: added the draculR hex sticker to the import your data tab. Added new tabs to display the figures when you upload your own data. Add multi select radio buttons for miRs to drop.

---
+++ draculR.R
@@ -0,0 +1,299 @@
+library(dplyr)
+library(plyr)
+library(ggplot2)
+library(shiny)
+library(ggrepel)
+library(scales) # For percent_format()
+library(tidyr)
+library(readr)
+
+# import our distribution difference dataframe
+plotData_distDiff_dCq <- read_csv("www/plotData_distDiff_dCq.csv")
+
+# import the rank and distribtuion difference data for GSE118038
+rank_GSE118038 <- read_csv(file = "www/rank_GSE118038.csv")
+unlistDistributionDifference_GSE118038 <- read_csv(file = "www/unlist_distributionDifference_GSE118038.csv")
+GSE118038_info <- paste("The data containined in GSE118038 were obtained from",
+                        " NCBI GEO and originate from the article \"A preliminary study of micro-RNAs",
+                        " as minimally invasive biomarkers for the diagnosis of prostate cancer patients.\"",
+                        " J Exp Clin Cancer Res 2021 Feb 23;40(1):79.", " Notably the data was ",
+                        " obtained in a normalised table hence the \"Mature miRNA as a function of Read Counts\"",
+                        " plot is not as expected.", sep = "")
+
+rankDist_GSE118038 <- dplyr::full_join(rank_GSE118038, unlistDistributionDifference_GSE118038, by = "samplename") %>% 
+  dplyr::mutate(., project = rep("GSE118038", nrow(.)))
+
+# import the rank and distribtuion difference data for GSE105052
+rank_GSE105052 <- read_csv(file = "www/rank_GSE105052.csv")
+unlistDistributionDifference_GSE105052 <- read_csv(file = "www/unlist_distributionDifference_GSE105052.csv")
+GSE105052_info <- paste("The data containined in GSE105052 were obtained from",
+                        " NCBI GEO and originate from the article \"Small ",
+                        " RNA-seq analysis of circulating miRNAs to identify phenotypic variability in",
+                        " Friedreich's ataxia patients.\" Sci Data 2018 Mar 6;5:180021.", sep = "")
+
+rankDist_GSE105052 <- dplyr::full_join(rank_GSE105052, unlistDistributionDifference_GSE105052, by = "samplename") %>% 
+  dplyr::mutate(., project = rep("GSE105052", nrow(.)))
+
+# import the rank and distribtuion difference data for GSE151341
+rank_GSE151341 <- read_csv(file = "www/rank_GSE151341.csv")
+unlistDistributionDifference_GSE151341 <- read_csv(file = "www/unlist_distributionDifference_GSE151341.csv")
+GSE151341_info <- paste("The data containined in c were obtained from",
+                        " NCBI GEO and originate from the article \"Sequencing",
+                        " identifies a distinct signature of circulating microRNAs",
+                        " in early radiographic knee osteoarthritis.\"",
+                        " Osteoarthritis Cartilage 2020 Nov;28(11):1471-1481.", sep = "")
+
+rankDist_GSE151341 <- dplyr::full_join(rank_GSE151341, unlistDistributionDifference_GSE151341, by = "samplename") %>% 
+  dplyr::mutate(., project = rep("GSE151341", nrow(.)))
+
+ui <- fluidPage(navbarPage(title = "draculR",
+                           
+                           tabPanel("Instructions",
+                                    tags$h5("This app uses two methods to classify miR-Seq sequencing data from human plasma into risk groups for haemolysis contamination"),
+                                    tags$h5(HTML(paste(
+                                      "All code used to calculate data shown here is available at the following",
+                                      tags$a(href="https://github.com/mxhp75/haemolysis_maternaPlasma.git", "git repository")
+                                    ))),
+                                    tags$h5(HTML(paste(
+                                      "In case you have questions",
+                                      tags$a(href="mailto::melanie.smith@adelaide.edu.au", "email me")
+                                    )))
+                           ),
+                           
+                           tabPanel("Public Data Example",
+                                    br(),
+                                    tags$h5("Click one of these buttons to see the results"),
+                                    fluidRow(
+                                      column(12,
+                                             actionButton(inputId = "GSE118038", label = "GSE118038"),
+                                             actionButton(inputId = "GSE105052", label = "GSE105052"),
+                                             actionButton(inputId = "GSE151341", label = "GSE151341"))
+                                    ),
+                                    br(),
+                                    fluidRow(
+                                      column = 8, offset = 0,
+                                      textOutput("projectInfo")
+                                    ),
+                                    br(),
+                                    fluidRow(
+                                      column(6, offset = 0,
+                                             plotOutput("mature_miRNA")),
+                                      column(6, offset = 6,
+                                             plotOutput("distributionDifference"))
+                                    )
+                                    
+                           ),
+                           
+                           tabPanel("Import new data",
+                                    sidebarLayout(
+                                      sidebarPanel(
+                                        fileInput("rawDataFile","Upload the file"), # fileinput() function is used to get the file upload control option
+                                        helpText("Default max. file size is 5MB"),
+                                        tags$hr(),
+                                        h5(helpText("Select the read.table parameters below")),
+                                        checkboxInput(inputId = 'header',
+                                                      label = 'Header?',
+                                                      value = TRUE),
+                                        checkboxInput(inputId = "stringAsFactors",
+                                                      label = "stringAsFactors",
+                                                      value = FALSE),
+                                        br(),
+                                        radioButtons(inputId = 'sep',
+                                                     label = 'Separator',
+                                                     choices = c(Comma = ',',
+                                                                 Tab = '\t'),
+                                                     selected = ','),
+                                        h5(helpText("Select any miRNA that are DE between your groups")),
+                                        checkboxGroupInput(inputId = 'drop_miRs',
+                                                     label = "Drop?",
+                                                     choices = c("miR-106b-3p" = 'hsa-miR-106b-3p',
+                                                                 "miR-140-3p" = 'hsa-mir-140-3p',
+                                                                 "miR-186-5p" = 'hsa-miR-186-5p',
+                                                                 "miR-425-5p", 'hsa-miR-425-5p',
+                                                                 "miR-142-5p" = 'hsa-miR-142-5p',
+                                                                 "miR-532-5p" = 'hsa-miR-532-5p',
+                                                                 "miR-17-5p", 'hsa-miR-17-5p',
+                                                                 "miR-25-3p" = 'hsa-miR-25-3p',
+                                                                 "miR-363-3p" = 'hsa-miR-363-3p',
+                                                                 "miR-183-5p" = 'hsa-miR-183-5p',
+                                                                 "miR-660-5p" = 'hsa-miR-660-5p',
+                                                                 "miR-451a" = 'hsa-miR-451a',
+                                                                 "miR-19b-3p" = 'hsa-miR-19b-3p',
+                                                                 "miR-182-5p" = 'hsa-miR-182-5p',
+                                                                 "miR-30c-5p" = 'hsa-miR-30c-5p',
+                                                                 "miR-324-5p" = 'hsa-miR-324-5p',
+                                                                 "miR-191-5p" = 'hsa-miR-191-5p',
+                                                                 "miR-192-5p" = 'hsa-miR-192-5p',
+                                                                 "miR-20b-5p" = 'hsa-miR-20b-5p'))
+                                      ),
+                                      mainPanel(
+                                        uiOutput("tb")
+                                      )
+                                    )
+                           ),
+                            
+                           tabPanel("Machine Learning",
+                                    h3("This tab is currently ",
+                                       tags$img(src = "underConstruction.png",
+                                                    heigth = 300,
+                                                    width = 300)))
+                           
+))
+  
+
+server <- function(input, output) {
+  # This reactive function will take the inputs from UI.R and use them for read.table() to read the data from the file. It returns the dataset in the form of a dataframe.
+  # file$datapath -> gives the path of the file
+  data <- reactive({
+    file1 <- input$rawDataFile
+    if(is.null(file1)){return()} 
+    read.table(file = file1$datapath,
+               sep = input$sep,
+               header = input$header,
+               stringsAsFactors = input$stringAsFactors)
+    
+  })
+  
+  # this reactive output will plot according to the public data reactive buttons
+  plotDataPublic_miRNA <- reactiveValues(data = NULL)
+  
+  observeEvent(input$GSE118038, { plotDataPublic_miRNA$data <- rankDist_GSE118038 })
+  observeEvent(input$GSE105052, { plotDataPublic_miRNA$data <- rankDist_GSE105052 })
+  observeEvent(input$GSE151341, { plotDataPublic_miRNA$data <- rankDist_GSE151341 })
+  
+  output$projectInfo <- renderText({
+    paste("You have selected", plotDataPublic_miRNA$data$project[1], ".",get(paste(plotDataPublic_miRNA$data$project[1],"_info", sep = "")))
+  }
+  )
+  
+  output$mature_miRNA <- renderPlot({
+    
+    ggplot(data = plotDataPublic_miRNA$data,
+           aes(x = readCounts,
+               y = unique_miRs)) +
+      geom_point(aes(colour = haemoResult,
+                     size = 4)) +
+      scale_size(guide = "none") +
+      scale_x_continuous(name = "Filtered Read Counts",
+                         breaks = seq(round_any(min(plotDataPublic_miRNA$data$readCounts), 10, f = floor), max(plotDataPublic_miRNA$data$readCounts), 100000)) +
+      scale_y_continuous(name = "Mature miRNA Identified",
+                         breaks = seq(round_any(min(plotDataPublic_miRNA$data$unique_miRs), 10, f = floor), max(plotDataPublic_miRNA$data$unique_miRs), 50)) +
+      stat_smooth(method = 'loess',
+                  se = FALSE,
+                  size = 2) +
+      geom_text_repel(data = filter(plotDataPublic_miRNA$data, haemoResult == "Caution"),
+                      box.padding = 1,
+                      aes(label = samplename),
+                      show.legend = FALSE) +
+      ggtitle(paste(plotDataPublic_miRNA$data$project[1], ": Mature miRNA as a \n function of Read Counts", sep = "")) +
+      theme_bw(base_size = 16) +
+      theme(
+        axis.title.x = element_text(margin = unit(c(3, 0, 0, 0), "mm")),
+        axis.title.y = element_text(margin = unit(c(0, 3, 0, 0), "mm"))
+      ) +
+      theme(
+        axis.text.x = element_text(angle = 270, hjust = 1)
+        ) 
+    
+  })
+  
+  output$distributionDifference <- renderPlot({
+  
+    # plot as histogram side by side
+    p <- ggplot() +
+      geom_histogram(data = plotData_distDiff_dCq,
+                     aes(x = distributionDifference,
+                         fill = haemolysis,
+                         colour = haemolysis,
+                         y = 2*(..density..)/sum(..density..)),
+                     breaks = seq(0,5,0.1),
+                     alpha = 0.6, 
+                     position = "identity",
+                     lwd = 0.8) +
+      geom_histogram(
+        data = plotDataPublic_miRNA$data,
+        aes(x = distributionDifference,
+            fill = project,
+            colour = project,
+            y = 2*(..density..)/sum(..density..)),
+        breaks = seq(0,5,0.1),
+        alpha = 0.6, 
+        position = "identity",
+        lwd = 0.8) +
+      geom_vline(show.legend = FALSE,
+                 xintercept = 1.9,
+                 col = 2,
+                 lty = 2) +
+      scale_y_continuous(labels = percent_format()) +
+      labs(
+        title = "Distribution difference using final classifiers",
+        subtitle = "based on three classification groups",
+        x = "Distribution Difference",
+        y = "% samples"
+      ) +
+      theme_bw(base_size = 16)
+    
+    caution <- dim(filter(plotDataPublic_miRNA$data, haemoResult == "Caution"))
+    
+    p + annotate(
+      geom = "text",
+      x = 3,
+      y = .23,
+      label = paste("we have identified", caution[1], "samples to use with caution", sep = " "),
+      colour = "red"
+    )
+    
+  })
+  
+  ## Info for the Import New Data tab
+  # this reactive output contains the summary of the dataset and display the summary in table format
+  output$about <- renderTable({
+    if(is.null(data())){return ()}
+    input$rawDataFile
+  })
+  
+  # this reactive output contains the summary of the dataset and display the summary in table format
+  output$sum <- renderTable({
+    if(is.null(data())){return ()}
+    summary(data())
+    
+  })
+  
+  # This reactive output contains the dataset and display the dataset in table format
+  output$table <- renderTable({
+    if(is.null(data())){return ()}
+    data()
+  })
+  
+  # This reactive output contains the dataset and display the figure
+  output$mature_mirna <- renderPlot({
+    if(is.null(data())){return ()}
+    data()
+  })
+  
+  # This reactive output contains the dataset and display the figure
+  output$dist_diff <- renderPlot({
+    if(is.null(data())){return ()}
+    data()
+  })
+  
+  # the following renderUI is used to dynamically generate the tabsets when the
+  # file is loaded. Until the file is loaded, app will not show the tabset.
+  output$tb <- renderUI({
+    if(is.null(data()))
+      h3("Test your own plasma data using ", tags$img(src = "drac.png",
+                                   heigth = 200,
+                                   width = 200))
+    else
+      tabsetPanel(tabPanel("About file", tableOutput("about")),
+                  tabPanel("Data", tableOutput("table")),
+                  tabPanel("Summary", tableOutput("sum")),
+                  tabPanel("Mature miRNA", plotOutput("mature_mirna")),
+                  tabPanel("Distribution Difference", plotOutput("dist_diff")))
+  })
+  
+   
+  
+}
+
+shinyApp(ui = ui, server = server)
